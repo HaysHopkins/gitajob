66aa092f6e9e43d145d1b7fa5e2485dd42224354
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2018-04-08 12:18:44 -0700

    Use VCR to remove external dependencies

--- spec/health_spec.rb
+++ spec/health_spec.rb
@@ -1,19 +1,21 @@
-require 'pry'
-
 RSpec.describe Gitajob::Health do
   let!(:website) { "https://about.gitlab.com" }
   let!(:repeat) { 1 }
   let!(:frequency) { 5 }
 
-  it "outputs website response to stdout", vcr: { cassette_name: "gitlab_response" } do
-    expect {
-      Gitajob::Health.new(website, repeat, frequency).probe
-    }.to output(/https:\/\/about.gitlab.com responded with/).to_stdout
+  it "outputs website response to stdout" do
+    VCR.use_cassette("gitlab_response") do
+      expect {
+        Gitajob::Health.new(website, repeat, frequency).probe
+      }.to output(/https:\/\/about.gitlab.com responded with/).to_stdout
+    end
   end
 
-  it "outputs average request time to stdout", vcr: { cassette_name: "gitlab_response" } do
-    expect {
-      Gitajob::Health.new(website, repeat, frequency).probe
-    }.to output(/After probing https:\/\/about.gitlab.com 1 times over the course of 0 seconds, the average request time/).to_stdout
+  it "outputs average request time to stdout" do
+    VCR.use_cassette("gitlab_response") do
+      expect {
+        Gitajob::Health.new(website, repeat, frequency).probe
+      }.to output(/After probing https:\/\/about.gitlab.com 1 times over the course of 0 seconds, the average request time/).to_stdout
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "bundler/setup"
 require "gitajob"
+require "webmock/rspec"
+require "vcr"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,4 +13,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.default_cassette_options = {record: :once}
+  config.hook_into :webmock
 end
